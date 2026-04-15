26e4c58170b329edd65cd35aaed1c51ef1f9da6f
Anupom Das <anudasrpmc39@gmail.com>
2026-04-16 00:09:36 +0200

    Add new drugs to the drugs list

--- js/drugs.js
+++ js/drugs.js
@@ -1,4 +1,7 @@
 const drugs = [
   {name:"Paracetamol",dose:15,freq:4,max:60,mg5:120},
-  {name:"Amoxicillin",dose:25,freq:3,max:1000,mg5:125}
+  {name:"Ibuprofen",dose:10,freq:3,max:40,mg5:100},
+  {name:"Amoxicillin",dose:25,freq:3,max:1000,mg5:125},
+  {name:"Azithromycin",dose:10,freq:1,max:500,mg5:200},
+  {name:"Cefixime",dose:8,freq:2,max:400,mg5:100}
 ];
